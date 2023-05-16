a83e9a6e8861d1b7840e22261d87c176182671b4
Er. Divyesh Shani <div3791@gmail.com>
2023-05-16 22:42:25 +0530

    generic

--- test/tree/traversal_test.dart
+++ test/tree/traversal_test.dart
@@ -5,19 +5,19 @@ import '../../bin/tree/height.dart';
 import '../../bin/tree/traversal.dart';
 
 void main() {
-  late TreeNode? root;
+  late TreeNode<int>? root;
 
   group(
     'tree traversal',
     () {
       setUp(() {
-        root = TreeNode(1);
-        TreeNode l1 = TreeNode(2);
-        TreeNode r1 = TreeNode(3);
-        TreeNode l2 = TreeNode(4);
-        TreeNode r2 = TreeNode(5);
-        TreeNode l3 = TreeNode(6);
-        TreeNode r3 = TreeNode(7);
+        root = TreeNode<int>(1);
+        var l1 = TreeNode<int>(2);
+        var r1 = TreeNode<int>(3);
+        var l2 = TreeNode<int>(4);
+        var r2 = TreeNode<int>(5);
+        var l3 = TreeNode<int>(6);
+        var r3 = TreeNode<int>(7);
 
         root?.left = l1;
         root?.right = r1;
