d28da50ec9862d00f2ca0806afce8dff80efaa57
Divyesh Shani <div3791@gmail.com>
2023-06-02 17:21:09 +0530

    graph

--- lib/graph.dart
+++ lib/graph.dart
@@ -1,5 +1,5 @@
 class Graph {
-  List<List<int>> _graph = [];
+  final List<List<int>> _graph = [];
   Graph(int vertex, List<List<int>> edges) {
     for (int i = 0; i < vertex; i++) {
       _graph.add([]);
