98ccb7751288140f711f14e6292523224e32cde2
Er. Divyesh Shani <div3791@gmail.com>
2023-06-23 20:42:54 +0530

    notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+void main(List<String> args) {
+  var a = B();
+  a.play();
+}
 
+mixin A {
+  void playA() => print('playing A');
+  void play();
+}
+
+mixin C {
+  void playC() => print('playing C');
+  void play();
+}
+
+class B with A, C {
+  @override
+  void play() {}
+}
