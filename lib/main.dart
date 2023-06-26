9b300a9b77002e619135034976f27d189a5b46fd
Divyesh Shani <div3791@gmail.com>
2023-06-26 13:23:45 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-void main(List<String> args) {
-  final a = show(10);
-  print(a.first);
-}
+void main(List<String> args) {}
 
-Iterable<int> show(n) sync* {
-  print('Start');
-  for (int i = 0; i < n; i++) {
-    print('generating');
-    yield i;
-  }
-  print('end');
+class A {
+  int? a;
+  int? b;
+
+  A(int x, int y)
+      : a = x,
+        b = y;
 }
